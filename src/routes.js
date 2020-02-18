98549b4aff42c9252ead3dd0c4c398f250ea188b
Caio Silveira F de Proft <caio_proft@hotmail.com>
2020-02-18 15:08:13 -0300

    Criação do model de doadores

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import Donor from './app/model/Donor';
 
 const routes = new Router();
 
 routes.get('/', function (req, res) {
   return res.render("index.html")
-})
+});
+
+routes.get('/donors', function (req, res) {
+  return res.json({ msg: 'oi' })
+});
 
 export default routes;
